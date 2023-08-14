88dd1f0e16c27e837275cb1f25da81c068173855
AshanurHossain0 <ashanurhossain0@gmail.com>
2023-08-14 14:53:56 +0530

    first-commit

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import {Navbar,Container, FormControl,Nav, Dropdown, Badge} from 'react-bootstrap'
+import { FaShoppingCart } from "react-icons/fa";
+import { AiFillDelete } from "react-icons/ai";
+
+const Header = () => {
+  return (
+    <Navbar bg="dark" variant="dark" style={{height:80}}>
+        <Container>
+            <Navbar.Brand>
+                <a href="/">Shopping Cart</a>
+            </Navbar.Brand>
+            <Navbar.Text className='search'>
+                <FormControl className='m-auto' placeholder='Seacrh product' style={{width:500}}/>
+            </Navbar.Text>
+            <Nav>
+                <Dropdown alignRight>
+                    <Dropdown.Toggle variant='success'>
+                        <FaShoppingCart color="white" fontSize="25px" style={{marginRight:5}}/>
+                        <Badge>10</Badge>
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu style={{minWidth:370}}>
+                        <span style={{padding:10}}>Cart is empty!</span>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </Nav>
+        </Container>
+    </Navbar>
+  )
+}
+
+export default Header
